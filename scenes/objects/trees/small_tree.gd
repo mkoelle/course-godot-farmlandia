b4e7773dc00b01821abf633bb4663effafbb85ff
Mark Koelle <mkoelle@gmail.com>
2026-02-27 14:21:55 -0600

    logs fall everyone dies

--- scenes/objects/trees/small_tree.gd
+++ scenes/objects/trees/small_tree.gd
@@ -3,6 +3,8 @@ extends Sprite2D
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var damage_component: Damage_Component = $DamageComponent
 
+var log_scene = preload("res://scenes/objects/trees/log.tscn")
+
 func _ready():
 	damage_component.on_damage.connect(on_damage)
 	health_component.destroyed.connect(fall)
@@ -11,5 +13,11 @@ func on_damage(hit_damage: int) -> void:
 	health_component.apply_damage(hit_damage)
 
 func fall() -> void:
+	call_deferred("add_log")
 	print("Opps I fell over")
 	queue_free()
+
+func add_log()-> void:
+	var log = log_scene.instantiate() as Node2D
+	log.global_position = global_position
+	get_parent().add_child(log)
